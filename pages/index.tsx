2949126a311fcdf9d96bba361ca97ebfca1d643e
sachintha999 <sachinthadil53383@gmail.com>
2024-10-03 00:37:01 +0530

    homepage changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="text-9xl text-red-900">HomePage</div>
+    <div className="text-9xl text-red-900">HomePage new cxhange</div>
   )
 }
 
